d7aea550750e53640ba930e6a10d4b613895452b
Lautaro Roldan <lautaronicolasroldan@gmail.com>
2024-04-30 22:14:41 -0300

    cambios para el build de vercel

---
+++ api/index.ts
@@ -0,0 +1,25 @@
+// Description: This file contains the code to create a server that will scrape the ivrea website and return the releases.
+const express = require('express');
+const { chromium } = require('playwright');
+import { release } from "os";
+import { getRealeases } from "../index.mjs";
+const app = express()
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+const PORT = process.env.PORT || 3000
+
+app.get('/getAll', async (req, res) => {
+    try {
+        getRealeases().then((release) => res.json(release))
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
--- index.mjs
+++ index.mjs
@@ -1,57 +1,61 @@
 import { chromium } from "playwright"
 
-const browser = await chromium.launch({
-    headless: true
-})
+export async function getRealeases() {
 
-const page = await browser.newPage()
+    const browser = await chromium.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.goto('https://www.ivrea.com.ar/proximas.htm');
 
-await page.goto('https://www.ivrea.com.ar/proximas.htm')
+    const releases = await page.$$eval('body > table:nth-child(2) > tbody > tr > td > table > tbody > tr > td > table:nth-child(3) > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr', (rows) => {
 
-const releases = await page.$$eval('body > table:nth-child(2) > tbody > tr > td > table > tbody > tr > td > table:nth-child(3) > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr', (rows) => {
-    const releasesArray = []
-    let currentDate = ''
-    let currentReleases = []
-    rows.forEach((row) => {
-        const dateCell = row.querySelector('td[bgcolor="#384D5A"]')
-        const releaseCell = row.querySelectorAll('td[background="img/fondo-tomo.gif"]')
-        if (dateCell) {
-            currentDate = dateCell.textContent.trim()
-            currentReleases = []
-        } else if (releaseCell.length > 0) {
-            currentReleases = []
-            releaseCell.forEach((releaseInfoCell) => {
-                const titleElement = releaseInfoCell.querySelector('strong')
-                const imageElement = releaseInfoCell.querySelector('.borde_fotos')
-                if (!titleElement) return
-                const title = titleElement.textContent.trim()
-                if (!title) return
-                if (!imageElement) return
-                const imgSrc = imageElement ? imageElement.getAttribute('src') : null
+        const releasesArray = []
+        let currentDate = ''
+        let currentReleases = []
+        let currentReeditions = []
+        rows.forEach((row) => {
+            const titleCell = row.querySelector('td[bgcolor="#384D5A"]')
+            const releaseCell = row.querySelectorAll('td[background="img/fondo-tomo.gif"]')
+            if (titleCell) {
+                currentDate = titleCell.textContent.trim()
+                currentReleases = []
+                currentReeditions = []
+            } else if (releaseCell.length > 0) {
+                currentReleases = []
+                currentReeditions = []
+                releaseCell.forEach((releaseInfoCell) => {
+                    const titleElement = releaseInfoCell.querySelector('strong')
+                    const imageElement = releaseInfoCell.querySelector('.borde_fotos')
+                    const reeditionElement = releaseInfoCell.querySelector('.pie')
+                    if (!titleElement) return
+                    const title = titleElement.textContent.trim()
+                    if (!imageElement) return
+                    const imgSrc = imageElement ? imageElement.getAttribute('src') : null
+                    if (!title) return
+                    if (reeditionElement) {
+                        currentReeditions.push({ title, imgSrc })
+                    } else if (currentReleases) {
+                        currentReleases.push({ title, imgSrc })
+                    }
 
-                currentReleases.push({ title, imgSrc })
-            })
-        }
-        if (currentDate && currentReleases.length > 0) {
-            // Si tenemos una fecha y lanzamientos, los agregamos al array
-            const findReleases = releasesArray.find((release) => release.date == currentDate)
-            if (findReleases) {
-                findReleases.releases = [...findReleases.releases, ...currentReleases]
-            } else {
-                releasesArray.push({ date: currentDate, releases: currentReleases })
+                })
+            }
+            if (currentDate && (currentReleases && currentReleases.length > 0) || (currentReeditions && currentReeditions.length > 0)) {
+                // Si tenemos una fecha y lanzamientos, los agregamos al array
+                const findReleases = releasesArray.find((release) => release.date == currentDate)
+                if (findReleases != null) {
+                    findReleases.releases = [...findReleases.releases, ...currentReleases]
+                    findReleases.reeditions = [...findReleases.reeditions, ...currentReeditions]
+                } else {
+                    const newReleases = { date: currentDate, releases: currentReleases, reeditions: currentReeditions }
+                    releasesArray.push(newReleases)
+                }
+                currentReleases = []
+                currentReeditions = []
             }
 
-        }
+        })
+        return { releasesArray }
     })
-    return releasesArray
-})
-
-
-
-releases.forEach((release) => {
-    console.log(release.date)
-    release.releases.forEach((releaseInfo) => {
-        console.log(releaseInfo.title, releaseInfo.imgSrc)
-    })
-})
-await browser.close()
+    await browser.close()
+    return releases
+}
